cf5b8cbfd3ae7a2cb60f361b142422cb3849ba58
M-Farag <mina.farag@icloud.com>
2023-03-29 23:23:39 +0200

    UPD Config struct to have a validation in place against the arguments
    ADD unit tests

--- src/lib.rs
+++ src/lib.rs
@@ -7,7 +7,16 @@ pub struct Config{
 impl Config {
     pub fn new(arguments:&Vec<String>) -> Self
     {
+        if arguments.len() < 3 
+        {
+            panic!("Expecting arguments to be at least 3");
+        }
         Self { file_path:arguments[1].clone(), query:arguments[2].clone() }
+    }
+
+    pub fn query(&self) -> &String
+    {
+        &self.query
     }
 
     pub fn file_path(&self) -> &String
@@ -20,5 +29,23 @@ impl Config {
 #[cfg(test)]
 mod main_lib_tests {
     use super::*;
+
+    #[test]
+    fn test_struct_config_constructor_works()
+    {
+        let args:Vec<String> = vec![String::from("0"),String::from("1"),String::from("2")];
+        let config = Config::new(&args);
+
+        assert_eq!(&String::from("1"),config.file_path());
+        assert_eq!(&String::from("2"),config.query());
+    }
+
+    #[test]
+    #[should_panic(expected="Expecting arguments to be at least 3")]
+    fn test_config_constructor_panics_with_fewer_arguments() 
+    {
+        let args:Vec<String> = vec![];
+        let config = Config::new(&args);
+    }
 
 }
