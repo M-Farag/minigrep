e3ce8ed1f0af01acc225242fdbcda7026e3045ea
M-Farag <mina.farag@icloud.com>
2023-03-30 22:41:09 +0200

    UPD Error handling within the main/run rs

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::env;
+use std::error::Error;
 use std::fs;
 use std::process;
 use minigrep as mg;
@@ -11,8 +12,17 @@ fn main(){
         }
     );
 
+    // we used if let because we don't care about the Ok() case as it returns a unit type
+    if let Err(e) = run(config) {
+        println!("Error is: {}",e);
+        process::exit(1);
+    }
 
-    let contents = fs::read_to_string(config.file_path());
+}
+
+fn run(config:mg::Config) -> Result<(),Box<dyn Error>>
+{
+    let contents = fs::read_to_string(config.file_path())?;
     println!("Contents are: {:#?}",contents);
-
+    Ok(())
 }
