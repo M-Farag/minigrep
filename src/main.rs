2c4f2f64c8c974432b0bbe588d4e6a234b63ae77
M-Farag <mina.farag@icloud.com>
2023-03-30 22:22:45 +0200

    UPD Refactor main to have a closure instead of panicing

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,15 @@
 use std::env;
 use std::fs;
+use std::process;
 use minigrep as mg;
 fn main(){
     let arguments:Vec<String> = env::args().collect();
-    let config = mg::Config::new(&arguments).unwrap();
+    let config = mg::Config::new(&arguments).unwrap_or_else(
+        |err| {
+            println!("Error is: {}",err);
+            process::exit(1);
+        }
+    );
 
 
     let contents = fs::read_to_string(config.file_path());
